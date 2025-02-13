244140699be52201aed966a2540dfc6025258a5f
d3ntryy <steamwasdqerg1234@gmail.com>
2025-02-13 23:17:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Нет(?",
+    "Серьезно нет??",
+    "Мне грустно?",
+    "НУ пожалуйста...",
+    "Подумай обо мне!",
+    "Если скажешь нет я буду грустить...",
+    "Очень...",
+    "Очень очень.",
+    "Ну пожалуста.",
+    "позязя! ❤️"
 ];
 
 let messageIndex = 0;
